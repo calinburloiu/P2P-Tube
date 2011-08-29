966a0fcfd6173580b114adce83cf16ccbfbc82df
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2011-08-29 10:38:04 +0300

    Update install plugins page (more links and a bit of rephrasing).

--- application/views/article/english/install-plugins.php
+++ application/views/article/english/install-plugins.php
@@ -5,16 +5,21 @@
 </ol>
 
 <h1><a name="SwarmPlayer">SwarmPlayer</a></h1>
-<p>To install SwarmPlayer video plugin which is based on HTML5 visit P2P-Next's <a href="http://swarmplayer.p2p-next.org/" target="_blank">SwarmPlayer Site</a>. You should find a green button there labeled "Add Swarmplayer...", click it and follow the instructions.</p>
-<a href="http://swarmplayer.p2p-next.org/" target="_blank"><img src="<?php echo site_url('img/SwarmPlayer-site.jpg') ?>" alt="SwarmPlayer Site" width="288" height="240" /></a>
+<p>To install the SwarmPlayer video plugin (based on <a href="http://www.w3.org/TR/html5/">HTML5</a>) visit P2P-Next's <a href="http://swarmplayer.p2p-next.org/" target="_blank">SwarmPlayer Site</a>. You'll find a page such as the one in the screenshot below.
+</p>
+
+<a href="http://swarmplayer.p2p-next.org/" target="_blank"><img src="<?php echo site_url('img/SwarmPlayer-site.jpg') ?>" alt="SwarmPlayer Site" width="288" height="240" align="center" /></a>
+
+<p>
+Find the green button labeled "Add Swarmplayer ...". Click it and follow the instructions.</p>
 
 <h1><a name="NextSharePC">NextSharePC</a></h1>
-<p>To install NextSharePC video plugin which is based on VLC do the following:</p>
+<p>To install the NextSharePC video plugin (based on <a href="http://www.videolan.org/vlc/">VLC</a>) follow the steps below:</p>
 <ol>
-	<li>If you are using:
-		<p><img src="<?php echo site_url('img/windows-icon.png') ?>" alt="win" class="inline" /> Windows run <a href="download/NextSharePC.exe">the Windows installer</a>.</p
-		<p><img src="<?php echo site_url('img/macosx-icon.png') ?>" alt="win" class="inline" /> a Mac run <a href="download/NextSharePC.dmg">the Mac OS X installer</a>.</p
+	<li>If you are using ...
+		<p><img src="<?php echo site_url('img/windows-icon.png') ?>" alt="win" class="inline" /> ... Windows, run <a href="download/NextSharePC.exe">the Windows installer</a>.</p>
+		<p><img src="<?php echo site_url('img/macosx-icon.png') ?>" alt="win" class="inline" /> ... Mac, run <a href="download/NextSharePC.dmg">the Mac OS X installer</a>.</p>
 	</li>
 	<li>Restart your browser and revisit the page.</li>
-	<li>Enjoy! ;)</li>
+	<li>Enjoy! ;-)</li>
 </ol>
