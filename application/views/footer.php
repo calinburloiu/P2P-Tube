b4a28ad3ca6e0166cbf209000277200c43d63802
Calin Burloiu <calin.burloiu@gmail.com>
2011-09-02 11:37:35 +0300

    footer modified for SEO

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,3 @@
 <div id="footer">
-	<div>Copyright &copy; 2011, <a href="http://www.upb.ro/">University POLITEHNICA of Bucharest</a>, <a href="http://www.p2p-next.org/">P2P-Next Project</a></div>
+	<div>Copyright &copy; 2011, <a href="http://www.upb.ro/">University POLITEHNICA of Bucharest (UPB)</a>, <a href="http://www.p2p-next.org/">P2P-Next Project</a></div>
 </div>
